e6c3b08bb46bb18e6b5aa3961b967ba574564aae
Minseo Kim <mskimahc@gmail.com>
2024-03-16 18:03:08 -0400

    hi

--- SignBridge/App.js
+++ SignBridge/App.js
@@ -1,10 +1,18 @@
+import React, { useState,  useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [hasCamreaPermission, setHasCameraPermission] = useState(null);
+  const [image, setImage] =  useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [flash, setFlash] = useState(Camera.Constants.Flashmode.off);
+  const cameraRef = useRef(null);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>An app that translates sign languages into English!</Text>
       <StatusBar style="auto" />
     </View>
   );
