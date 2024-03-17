5e1fe3a2e51dd5ef6103d114c7ad6ad50e9b6803
Minseo Kim <mskimahc@gmail.com>
2024-03-16 22:49:56 -0400

    camera

--- SignBridge/App.js
+++ SignBridge/App.js
@@ -1,19 +1,37 @@
-import React, { useState,  useEffect} from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
+import React, { useState,  useEffect, useRef} from 'react';
+import { Camera, CameraType } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   const [hasCamreaPermission, setHasCameraPermission] = useState(null);
   const [image, setImage] =  useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
-  const [flash, setFlash] = useState(Camera.Constants.Flashmode.off);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
   const cameraRef = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      MediaLibrary.requestPermissionsAsync();
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === 'granted');
+    })();
+  }, [])
 
   return (
     <View style={styles.container}>
-      <Text>An app that translates sign languages into English!</Text>
-      <StatusBar style="auto" />
+      <Camera
+        style={styles.camera}
+        type={type}
+        flashMode={flash}
+        ref={cameraRef}
+      >
+        <Text>hello</Text>
+      </Camera>
+      <View>
+        
+      </View>
+  
     </View>
   );
 }
@@ -22,7 +40,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  camera: {
+    flex: 1,
+    borderRadius: 20, 
+  }
 });
